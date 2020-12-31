65f110b8ecfe9b7fc7dc474004adcff1a67864df
Eddy <eddy@Eddys-MBP.lan>
2020-12-31 18:05:41 -0500

    added app.js and index.js index houses app.js and then finds the id in the html page to serve up the app itself

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <>
+                <div className="AppContainer"> </div>
+
+
+                <div> testing App Component </div>
+            
+
+            </>
+        );
+    }
+
+};
+
+export default App;
