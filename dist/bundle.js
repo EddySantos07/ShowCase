295bdb44f7a0e243a57e6273489f751b02511216
Eddy <eddy@Eddys-MBP.lan>
2020-12-31 18:21:09 -0500

    added  start command to start webpack then added .babelrc from visual algo to set up babel to compile

---
+++ dist/bundle.js
@@ -0,0 +1,15 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: /Users/eddy/Projects/Showcase/src/index.js: Support for the experimental syntax 'jsx' isn't currently enabled (5:18):\\n\\n\\u001b[0m \\u001b[90m 3 | \\u001b[39m\\u001b[36mimport\\u001b[39m \\u001b[33mApp\\u001b[39m from \\u001b[32m\\\"./App.js\\\"\\u001b[39m\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 4 | \\u001b[39m\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 5 | \\u001b[39m\\u001b[33mReactDOM\\u001b[39m\\u001b[33m.\\u001b[39mrender( \\u001b[33m<\\u001b[39m\\u001b[33mApp\\u001b[39m \\u001b[33m/\\u001b[39m\\u001b[33m>\\u001b[39m \\u001b[33m,\\u001b[39m document\\u001b[33m.\\u001b[39mgetElementById(\\u001b[32m'App'\\u001b[39m) )\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m   | \\u001b[39m                 \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\nAdd @babel/preset-react (https://git.io/JfeDR) to the 'presets' section of your Babel config to enable transformation.\\nIf you want to leave it as-is, add @babel/plugin-syntax-jsx (https://git.io/vb4yA) to the 'plugins' section to enable parsing.\\n    at Parser._raise (/Users/eddy/Projects/Showcase/node_modules/@babel/parser/lib/index.js:748:17)\\n    at Parser.raiseWithData (/Users/eddy/Projects/Showcase/node_modules/@babel/parser/lib/index.js:741:17)\\n    at Parser.expectOnePlugin (/Users/eddy/Projects/Showcase/node_modules/@babel/parser/lib/index.js:9116:18)\\n    at Parser.parseExprAtom (/Users/eddy/Projects/Showcase/node_modules/@babel/parser/lib/index.js:10569:22)\\n    at Parser.parseExprSubscripts (/Users/eddy/Projects/Showcase/node_modules/@babel/parser/lib/index.js:10150:23)\\n    at Parser.parseUpdate (/Users/eddy/Projects/Showcase/node_modules/@babel/parser/lib/index.js:10130:21)\\n    at Parser.parseMaybeUnary (/Users/eddy/Projects/Showcase/node_modules/@babel/parser/lib/index.js:10119:17)\\n    at Parser.parseExprOps (/Users/eddy/Projects/Showcase/node_modules/@babel/parser/lib/index.js:9989:23)\\n    at Parser.parseMaybeConditional (/Users/eddy/Projects/Showcase/node_modules/@babel/parser/lib/index.js:9963:23)\\n    at Parser.parseMaybeAssign (/Users/eddy/Projects/Showcase/node_modules/@babel/parser/lib/index.js:9926:21)\");\n\n//# sourceURL=webpack://showcase/./src/index.js?");
+/******/ })()
+;
