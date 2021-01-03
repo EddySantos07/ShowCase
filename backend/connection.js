0bf21ac12005b105f1809bd7f07ebdd10513a720
Eddy <eddy@Eddys-MBP.lan>
2021-01-02 22:20:00 -0500

    added db connection and fixed .env folder

--- backend/connection.js
+++ backend/connection.js
@@ -1,4 +1,27 @@
 const mongoose = require('mongoose');
+
+// require dotenv
 require('dotenv').config();
 
-// mongoose.connect( `mongodb+srv://Eddy:${PASSWORD}@showcase.jaglz.mongodb.net/${DB}?retryWrites=true&w=majority`, )
+//get enviorment passwords
+const PASSWORD = process.env.PASSWORD;
+const DB = process.env.DB;
+
+mongoose.connect(`mongodb+srv://Eddy:${PASSWORD}@showcase.jaglz.mongodb.net/${DB}?retryWrites=true&w=majority`, { useNewUrlParser: true } );
+
+const db = mongoose.connection;
+
+// checking for errs
+db.on('error', function () {
+    console.log('connection err');
+});
+
+db.once('open', function () {
+    console.log('DB Connected!');
+});
+
+const projectSchema = new mongoose.Schema({
+    name: String,
+    difficulty: Number,
+});
+
--- src/components/DisplayProjects/GetProjects.js
+++ src/components/DisplayProjects/GetProjects.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function GetProjects () {
+
+
 
   return (
       <div className="ProjectHouseContainer"> house projects
